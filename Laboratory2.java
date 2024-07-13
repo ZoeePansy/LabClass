b09f456131711e4f73a1ecf553f39c3a7c324efa
ZoeePansy <124904669+ZoeePansy@users.noreply.github.com>
2024-07-13 09:57:16 +0700

    Add files via upload

---
+++ Laboratory2.java
@@ -0,0 +1,15 @@
+public class Laboratory2 {
+    public static void main(String args[]) {
+        int A = 5;
+        int B = 11;
+        double C = 99;
+        int D = 29;
+        int E = 8;
+        int F = 1;
+        double G = 5.5;
+        int H = 3;
+        System.out.println(A+B*C/(D*E)-(F+G)-H);
+
+    
+    }
+}
